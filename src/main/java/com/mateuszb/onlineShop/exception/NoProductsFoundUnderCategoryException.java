7aaaa24f93d8b5674e70ca68964615d63cf631a6
epsonik <epsonik@o2.pl>
2016-09-28 01:05:33 +0200

    koszyk

---
+++ src/main/java/com/mateuszb/onlineShop/exception/NoProductsFoundUnderCategoryException.java
@@ -0,0 +1,10 @@
+package com.mateuszb.onlineShop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Brak produkt�w we wskazanej kategorii")
+public class NoProductsFoundUnderCategoryException extends RuntimeException{
+
+	private static final long serialVersionUID = 3935230281455340039L;
+}
